140811812fdb23e79078ad64e07e49812e2ae30f
JaredVahle <jaredjohnvahle@gmail.com>
2021-09-03 13:49:45 -0500

    updating work

---
+++ telco_acquire_prep.py
@@ -0,0 +1,74 @@
+import env
+import os
+import pandas as pd
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+
+
+def get_telco_data():
+    filename = "telco_churn.csv"
+
+    if os.path.isfile("telco_churn.csv"):
+        return pd.read_csv("telco_churn.csv")
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('''
+SELECT *
+FROM customers
+JOIN contract_types ON contract_types.contract_type_id = customers.contract_type_id
+JOIN internet_service_types ON internet_service_types.internet_service_type_id = customers.internet_service_type_id
+JOIN payment_types ON payment_types.payment_type_id = customers.payment_type_id;
+''', get_connection("telco_churn"))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv("telco_churn.csv")
+
+        # Return the dataframe to the calling code
+        return df 
+
+def clean_telco(df):
+    df.total_charges.replace(to_replace = {" ":"0"}, inplace = True)
+    df.total_charges = df.total_charges.astype("float")
+    df["auto_pay"] = df.payment_type.str.contains("auto")
+    df["auto_pay"] = df.auto_pay.replace(to_replace = [True,False],value = [1,0])
+    dummy_df = pd.get_dummies(df[['gender', 'payment_type',"internet_service_type","contract_type"]])
+    dummy_df.columns = [col.lower().replace(" ","_") for col in dummy_df]
+    
+    df["partner"] = df.partner.replace(to_replace = ["Yes","No"],value = [1,0])
+    df["dependents"] = df.dependents.replace(to_replace = ["Yes","No"],value = [1,0])
+    df["churn"] = df.churn.replace(to_replace = ["Yes","No"],value = [1,0])
+    df["multiple_lines"] = df.paperless_billing.replace(to_replace = ["Yes","No","No phone service"],value = [1,0,0])
+    df["paperless_billing"] = df.paperless_billing.replace(to_replace = ["Yes","No","No internet service"],value = [1,0,0])
+    df["tech_support"] = df.tech_support.replace(to_replace = ["Yes","No","No internet service"],value = [1,0,0])
+    df["device_protection"] = df.device_protection.replace(to_replace = ["Yes","No","No internet service"],value = [1,0,0])
+    df["online_backup"] = df.online_backup.replace(to_replace = ["Yes","No","No internet service"],value = [1,0,0])
+    df["online_security"] = df.online_security.replace(to_replace = ["Yes","No","No internet service"],value = [1,0,0])
+    df["streaming_tv"] = df.streaming_tv.replace(to_replace = ["Yes","No","No internet service"],value = [1,0,0])
+    df["streaming_movies"] = df.streaming_movies.replace(to_replace = ["Yes","No","No internet service"],value = [1,0,0])
+    df["phone_service"] = df.phone_service.replace(to_replace = ["Yes","No","No internet service"],value = [1,0,0])
+    df["multiple_lines"] = df.multiple_lines.replace(to_replace = ["Yes","No","No phone service"],value = [1,0,0])
+    df["add_ons"] =  df.online_security + df.online_backup + df.device_protection + df.tech_support + df.streaming_tv + df.streaming_movies
+    df.drop(columns = ["customer_id",
+                       "gender",
+                       "payment_type",
+                       "internet_service_type",
+                       "contract_type",
+                   'internet_service_type_id',
+                   "contract_type_id",
+                   "payment_type_id",
+                  "contract_type_id.1",
+                   "internet_service_type_id.1",
+                   "payment_type_id.1",
+                   "Unnamed: 0"],inplace = True)
+    return pd.concat([df, dummy_df], axis=1)
+
+def split_telco_data(df):
+    """
+    splits the data in train validate and test 
+    """
+    train, test = train_test_split(df, test_size = 0.2, random_state = 123, stratify = df.survived)
+    train, validate = train_test_split(train, test_size=.25, random_state=123, stratify=train.survived)
+    
+    return train, validate, test
