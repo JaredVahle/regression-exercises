1391e7a1484141b29c83c608d265753653bb38b7
JaredVahle <jaredjohnvahle@gmail.com>
2021-09-07 11:01:00 -0500

    updating prepare.py

--- prepare.py
+++ prepare.py
@@ -6,12 +6,28 @@ import numpy as np
 import sklearn.preprocessing
 from sklearn.model_selection import train_test_split
 
-def scale_telco_data(train,validate,test):
-    scaler = sklearn.preprocessing.StandardScaler()
-    scaler.fit(train[["monthly_charges","total_charges"]])
+def Standard_Scaler(X_train, X_validate, X_test):
+    """
+    Takes in X_train, X_validate and X_test dfs with numeric values only
+    Returns scaler, X_train_scaled, X_validate_scaled, X_test_scaled dfs
+    """
 
-    train = scaler.transform(train[["monthly_charges","total_charges"]])
-    validate = scaler.transform(validate[["monthly_charges","total_charges"]])
-    test = scaler.transform(test[["monthly_charges","total_charges"]])
+    scaler = sklearn.preprocessing.StandardScaler().fit(X_train)
+    X_train_scaled = pd.DataFrame(scaler.transform(X_train), index = X_train.index, columns = X_train.columns)
+    X_validate_scaled = pd.DataFrame(scaler.transform(X_validate), index = X_validate.index, columns = X_validate.columns)
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test), index = X_test.index, columns = X_test.columns)
     
-    return train,validate,test
+    return scaler, X_train_scaled, X_validate_scaled, X_test_scaled
+
+
+def Min_Max_Scaler(X_train, X_validate, X_test):
+    """
+    Takes in X_train, X_validate and X_test dfs with numeric values only
+    Returns scaler, X_train_scaled, X_validate_scaled, X_test_scaled dfs 
+    """
+    scaler = sklearn.preprocessing.MinMaxScaler().fit(X_train)
+    X_train_scaled = pd.DataFrame(scaler.transform(X_train), index = X_train.index, columns = X_train.columns)
+    X_validate_scaled = pd.DataFrame(scaler.transform(X_validate), index = X_validate.index, columns = X_validate.columns)
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test), index = X_test.index, columns = X_test.columns)
+    
+    return scaler, X_train_scaled, X_validate_scaled, X_test_scaled
